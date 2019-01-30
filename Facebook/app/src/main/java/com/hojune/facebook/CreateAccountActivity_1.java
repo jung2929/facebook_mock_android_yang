b0ed693ab8ec3243b59c79b03c1f8d1a03917b57
Yanghojun <hojune0303@naver.com>
2019-01-31 00:15:41 +0900

    회원가입, 로그인 연동/ 게시판, 타임라인 레이아웃 수정 예정

--- Facebook/app/src/main/java/com/hojune/facebook/CreateAccountActivity_1.java
+++ Facebook/app/src/main/java/com/hojune/facebook/CreateAccountActivity_1.java
@@ -5,6 +5,7 @@ import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 public class CreateAccountActivity_1 extends AppCompatActivity {
@@ -14,11 +15,17 @@ public class CreateAccountActivity_1 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account_1);
 
+
+        final EditText name1 = (EditText) findViewById(R.id.name1);
+        final EditText name2 = (EditText) findViewById(R.id.name2);
+
         ImageButton next = (ImageButton)findViewById(R.id.next);
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(CreateAccountActivity_1.this,CreateAccountActivity_2.class);
+                intent.putExtra("name1", name1.getText().toString());
+                intent.putExtra("name2", name2.getText().toString());
                 startActivity(intent);
             }
         });
--- Facebook/app/src/main/java/com/hojune/facebook/CreateAccountActivity_5.java
+++ Facebook/app/src/main/java/com/hojune/facebook/CreateAccountActivity_5.java
@@ -4,8 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
-import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 public class CreateAccountActivity_5 extends AppCompatActivity {
@@ -15,11 +14,23 @@ public class CreateAccountActivity_5 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account_5);
 
-        ImageButton confirm = (ImageButton) findViewById(R.id.confirm);
-        confirm.setOnClickListener(new View.OnClickListener() {
+        final Intent intent0 = getIntent();
+
+        final EditText pw = (EditText) findViewById(R.id.pw);
+
+        ImageButton next = (ImageButton)findViewById(R.id.next);
+        next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(CreateAccountActivity_5.this,MyProfileActivity.class);
+                Intent intent = new Intent(CreateAccountActivity_5.this,CreateAccountActivity_6.class);
+
+                intent.putExtra("name1",intent0.getStringExtra("name1").toString());
+                intent.putExtra("name2",intent0.getStringExtra("name2").toString());
+                intent.putExtra("bir",intent0.getStringExtra("bir").toString());
+                intent.putExtra("gender",intent0.getStringExtra("gender").toString());
+                intent.putExtra("id", intent0.getStringExtra("id"));
+                intent.putExtra("pw", pw.getText().toString());
+
                 startActivity(intent);
             }
         });
